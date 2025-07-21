894a9349e7afcf5ee7d14755440025b2853f7fec
Anthony Welte <tony.welte@gmail.com>
2025-07-21 18:59:26 +0000

    Rebase from 'upstream'

--- rosidlcpp_typesupport_introspection_cpp/src/rosidlcpp_typesupport_introspection_cpp/rosidlcpp_typesupport_introspection_cpp.cpp
+++ rosidlcpp_typesupport_introspection_cpp/src/rosidlcpp_typesupport_introspection_cpp/rosidlcpp_typesupport_introspection_cpp.cpp
@@ -29,7 +29,6 @@
 #include <cstdlib>
 #include <exception>
 #include <filesystem>
-#include <format>
 #include <iostream>
 #include <ostream>
 #include <string>
@@ -122,8 +121,8 @@ void GeneratorTypesupportIntrospectionCpp::run() {
     const auto msg_directory = ros_json["interface_path"]["filedir"].get<std::string>();
     const auto msg_type = ros_json["interface_path"]["filename"].get<std::string>();
     std::filesystem::create_directories(m_arguments.output_dir + "/" + msg_directory + "/detail");
-    write_template(template_idl, ros_json, std::format("{}/detail/{}__type_support.cpp", msg_directory, rosidlcpp_core::camel_to_snake(msg_type)));
-    write_template(template_idl_rosidl, ros_json, std::format("{}/detail/{}__rosidl_typesupport_introspection_cpp.hpp", msg_directory, rosidlcpp_core::camel_to_snake(msg_type)));
+    write_template(template_idl, ros_json, fmt::format("{}/detail/{}__type_support.cpp", msg_directory, rosidlcpp_core::camel_to_snake(msg_type)));
+    write_template(template_idl_rosidl, ros_json, fmt::format("{}/detail/{}__rosidl_typesupport_introspection_cpp.hpp", msg_directory, rosidlcpp_core::camel_to_snake(msg_type)));
   }
 }
 
