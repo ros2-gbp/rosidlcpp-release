1768b7919a038063583cf32e2a49c4ceb1504a07
Anthony Welte <tony.welte@gmail.com>
2026-01-26 21:14:36 +0000

    Rebase from 'upstream'

--- rosidlcpp_typesupport_c/cmake/rosidlcpp_typesupport_c_generate_interfaces.cmake
+++ rosidlcpp_typesupport_c/cmake/rosidlcpp_typesupport_c_generate_interfaces.cmake
@@ -70,6 +70,12 @@ rosidl_write_generator_arguments(
   TARGET_DEPENDENCIES ${target_dependencies}
 )
 
+if(${CMAKE_VERSION} VERSION_GREATER_EQUAL 3.27)
+  set(_dep_explicit_only DEPENDS_EXPLICIT_ONLY)
+else()
+  set(_dep_explicit_only "")
+endif()
+
 get_used_typesupports(typesupports "rosidl_typesupport_c")
 add_custom_command(
   OUTPUT ${_generated_sources}
@@ -80,6 +86,7 @@ add_custom_command(
   DEPENDS ${target_dependencies}
   COMMENT "Generating C type support dispatch for ROS interfaces"
   VERBATIM
+  ${_dep_explicit_only}
 )
 
 set(_target_suffix "__rosidl_typesupport_c")
