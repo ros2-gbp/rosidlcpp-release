2c0611e54929fa951420903510a2668c11007965
Anthony Welte <tony.welte@gmail.com>
2026-05-18 21:50:10 +0000

    Revert "Trimmed the branch to only the rosidlcpp_typesupport_introspection_c sub directory"
    This reverts commit aae47dfb09ccc845b5ae072a72a4ba4991472b0b.

---
+++ rosidlcpp_generator_py/include/rosidlcpp_generator_py/rosidlcpp_generator_py.hpp
@@ -0,0 +1,31 @@
+// Copyright 2025 Anthony Welte
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <rosidlcpp_generator_core/generator_base.hpp>
+
+#include <string>
+#include <vector>
+
+class GeneratorPython : public rosidlcpp_core::GeneratorBase {
+ public:
+  GeneratorPython(rosidlcpp_core::GeneratorArguments generator_arguments, std::vector<std::string> typesupport_implementations_list);
+
+  void run();
+
+ private:
+  rosidlcpp_core::GeneratorArguments m_arguments;
+  std::vector<std::string> m_typesupport_implementations;
+};
